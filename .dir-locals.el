d32df2b6740f496b429ee66bc1c1b48ed5c147d3
Matti Uusitalo <matti.uusitalo@metosin.fi>
2025-04-17 09:45:58 +0300

    Add needed options

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,4 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((cider-clojure-cli-aliases . ":dev:backend")
-         (cider-default-cljs-repl . shadow)
-         (cider-shadow-default-options . ":app")
-         (cider-shadow-cljs-watched-builds . ("app")))))
+((nil . ((cider-clojure-cli-aliases . ":dev")
